1ea5e6902fe6d2db6a7ca6281027a0794382a438
Okechukwu Ugwu <fobilow@gmail.com>
2022-05-11 15:38:27 +0100

    Extend CaptureRequest (#3)
    Add BillPayer and Meta fields

--- psp/capture.go
+++ psp/capture.go
@@ -7,6 +7,8 @@ type CaptureRequest struct {
 	Amount            Amount            `json:"amount"`
 	BillingProfileID  string            `json:"billingProfileId"`
 	PaymentInstrument PaymentInstrument `json:"paymentInstrument"`
+	BillPayer         Person            `json:"billPayer"`
+	Meta              Meta              `json:"meta"`
 }
 
 type CaptureResponse struct {
