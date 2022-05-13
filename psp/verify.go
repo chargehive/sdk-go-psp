ba641e83a83db2ed0f082e939345d9096c8aeb45
Okechukwu Ugwu <fobilow@gmail.com>
2022-05-13 12:16:57 +0100

    Add VerifyRequest & VerifyResponse

---
+++ psp/verify.go
@@ -0,0 +1,26 @@
+package psp
+
+import "encoding/json"
+
+type VerifyRequest struct {
+	Type              PaymentInstrumentVerifyType `json:"type"`  // Type is the type of verification we want to perform e.g. identify, challenge
+	Value             string                      `json:"value"` // Value to verify
+	BillingProfileID  string                      `json:"billingProfileId"`
+	PaymentInstrument PaymentInstrument           `json:"paymentInstrument"`
+}
+
+type VerifyResponse struct {
+	TransactionResponse
+}
+
+func (r VerifyRequest) GetPath(credentialID string) string {
+	return "/v1/" + credentialID + "/payment/verify"
+}
+
+func (r VerifyRequest) Do(conn Connection) (resp VoidResponse, err error) {
+	body, err := conn.Do(r)
+	if err == nil {
+		err = json.Unmarshal(body, &resp)
+	}
+	return
+}
