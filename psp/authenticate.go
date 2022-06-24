55978350792fff57a2172ddbaf64b7089f11fff1
Tom Kay <oridan82@gmail.com>
2022-06-24 10:20:51 +0100

    reinstate PaymentInstrumentAuthenticationType (#25)

--- psp/authenticate.go
+++ psp/authenticate.go
@@ -4,12 +4,20 @@ import (
 	"encoding/json"
 )
 
+type AuthenticateRequestType string
+
+//goland:noinspection GoUnusedConst
+const (
+	AuthenticateRequestTypeIdentify  AuthenticateRequestType = "identify"
+	AuthenticateRequestTypeChallenge AuthenticateRequestType = "challenge"
+)
+
 type AuthenticateRequest struct {
-	Type              PaymentInstrumentAuthenticationType `json:"type"` // Type is the type of authentication we want to perform e.g. identify, challenge
-	Amount            Amount                              `json:"amount"`
-	BillPayer         Person                              `json:"billPayer"`
-	BillingProfileID  string                              `json:"billingProfileId"`
-	PaymentInstrument PaymentInstrument                   `json:"paymentInstrument"`
+	Type              AuthenticateRequestType `json:"type"` // Type is the type of authentication we want to perform e.g. identify, challenge
+	Amount            Amount                  `json:"amount"`
+	BillPayer         Person                  `json:"billPayer"`
+	BillingProfileID  string                  `json:"billingProfileId"`
+	PaymentInstrument PaymentInstrument       `json:"paymentInstrument"`
 }
 
 type AuthenticateResponse struct {
