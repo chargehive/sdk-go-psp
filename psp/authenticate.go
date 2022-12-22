79b4ca5bb7f376d2b032bcde61ee66a60c61d82d
Okechukwu Ugwu <fobilow@gmail.com>
2022-12-22 14:15:19 +0100

    Add authenticate request and response (#47)

---
+++ psp/authenticate.go
@@ -0,0 +1,34 @@
+package psp
+
+import (
+	"encoding/json"
+)
+
+type AuthenticateRequest struct {
+	BaseTransactionRequest
+}
+
+type AuthenticateResponse struct {
+	TransactionResponse
+	ThreeDSResult *ThreeDSResult `json:"3dsResult"`
+}
+
+func (r *AuthenticateRequest) GetPath(credentialID string) string {
+	return "/v1/" + credentialID + "/payment/authenticate"
+}
+
+func (r *AuthenticateRequest) Do(conn Connection) (resp AuthenticateResponse, err error) {
+	body, _, err := conn.Do(r)
+	if err == nil {
+		err = json.Unmarshal(body, &resp)
+	}
+
+	return
+}
+
+func (r *AuthenticateRequest) SetCorrelationID(correlationID string) {
+	r.correlationID = correlationID
+}
+func (r *AuthenticateRequest) GetCorrelationID() string {
+	return r.correlationID
+}
