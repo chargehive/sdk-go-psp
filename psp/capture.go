babef6de59da356f3aeb4b3bc5b29549645f72b4
Tom Kay <oridan82@gmail.com>
2023-07-04 13:16:35 +0100

    Change AmountAuthorized to pointer and add response fallbacks
    Modified the `AmountAuthorized` field in `psp/capture.go` to be a pointer instead of a value object to accurately handle null values. Additionally, added fallbacks for response fields to ensure every field is correctly populated. This change addresses scenarios when certain transaction responses are missing, to prevent our system from processing incomplete data.

--- psp/capture.go
+++ psp/capture.go
@@ -20,7 +20,7 @@ type CaptureResponse struct {
 	Authorize            AuthorizeResponse
 	AuthorizeTransaction *TransactionResponse `json:"authorizeTransaction,omitempty"`
 	ThreeDSResult        *ThreeDSResult       `json:"3dsResult"`
-	AmountAuthorized     Amount               `json:"amountAuthorized"`
+	AmountAuthorized     *Amount              `json:"amountAuthorized"`
 	AuthCode             string               `json:"authCode"`
 	CVVResponse          string               `json:"cvvResponse"`
 	AVS                  string               `json:"avs"`
@@ -36,6 +36,29 @@ func (r *CaptureRequest) Do(conn Connection) (resp CaptureResponse, err error) {
 	if err == nil {
 		err = json.Unmarshal(body, &resp)
 		resp.RequestID = headers.Get(RequestHeaderRequestID)
+
+		if resp.AuthorizeTransaction == nil && resp.Authorize.TransactionResponse.TransactionID != "" {
+			resp.AuthorizeTransaction = &resp.Authorize.TransactionResponse
+		}
+		if resp.ThreeDSResult == nil && resp.Authorize.ThreeDSResult != nil {
+			resp.ThreeDSResult = resp.Authorize.ThreeDSResult
+		}
+		if resp.AmountAuthorized == nil && resp.Authorize.AmountAuthorized.Units != 0 {
+			resp.AmountAuthorized = &resp.Authorize.AmountAuthorized
+		}
+		if resp.AuthCode == "" && resp.Authorize.AuthCode != "" {
+			resp.AuthCode = resp.Authorize.AuthCode
+		}
+		if resp.CVVResponse == "" && resp.Authorize.CVVResponse != "" {
+			resp.CVVResponse = resp.Authorize.CVVResponse
+		}
+		if resp.AVS == "" && resp.Authorize.AVS != "" {
+			resp.AVS = resp.Authorize.AVS
+		}
+		if resp.ECI == "" && resp.Authorize.ECI != "" {
+			resp.ECI = resp.Authorize.ECI
+		}
+
 	}
 
 	return
