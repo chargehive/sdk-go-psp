db11042016db8159d8ba8ed02415057980a81b74
Oke Ugwu <fobilow@gmail.com>
2024-08-28 09:03:50 +0100

    restructure transaction response

--- psp/capture.go
+++ psp/capture.go
@@ -2,18 +2,11 @@ package psp
 
 import (
 	"encoding/json"
-	"time"
 )
 
 type CaptureRequest struct {
 	BaseTransactionRequest
 	ThreeDSResult *ThreeDSResult `json:"3dsResult"`
-
-	// Retry fields
-	RetryCount            int       `json:"retryCount"`
-	RetryReference        string    `json:"retryReference"`
-	RetryDate             time.Time `json:"retryDate"`
-	RetryFirstAttemptDate time.Time `json:"retryFirstAttemptDate"`
 }
 
 type CaptureResponse struct {
@@ -24,16 +17,6 @@ type CaptureResponse struct {
 	AuthorizeTransaction *TransactionResponse `json:"authorizeTransaction,omitempty"`
 	ThreeDSResult        *ThreeDSResult       `json:"3dsResult"`
 	AmountAuthorized     *Amount              `json:"amountAuthorized"`
-	AuthCode             string               `json:"authCode"`
-	CVVResponse          string               `json:"cvvResponse"`
-	AVS                  string               `json:"avs"`
-	ECI                  string               `json:"eci"`
-
-	// Retry fields
-	RetryCount            int       `json:"retryCount"`
-	RetryReference        string    `json:"retryReference"`
-	RetryDate             time.Time `json:"retryDate"`
-	RetryFirstAttemptDate time.Time `json:"retryFirstAttemptDate"`
 }
 
 func NewCaptureResponse(currency string) CaptureResponse {
